a9c22cc1dfb8132e8d5c68cc6d4110c8097dd533
Nicholas Triano <nicholastriano1999@gmail.com>
2022-06-03 17:38:07 -0400

    added cors and unknown endpoint response

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 3001
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
